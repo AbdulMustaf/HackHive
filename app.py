b8d0bbf8cbbe09fe47c31eb4166ff62f03a10e43
AbdulMustaf <abdullahmustafa300@gmail.com>
2025-02-08 12:59:19 -0500

    Create app.py

---
+++ app.py
@@ -0,0 +1,48 @@
+from flask import Flask, render_template, Response
+import cv2
+import numpy as np
+from tensorflow.keras.models import load_model
+
+
+app = Flask(__name__)
+model = load_model('violence_detection_model.h5')
+
+def generate_frames():
+    cap = cv2.VideoCapture(0)
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            break
+
+        resized_frame = cv2.resize(frame, (224, 224))
+        normalized_frame = resized_frame / 255.0
+        input_frame = np.expand_dims(normalized_frame, axis=0)
+        prediction = model.predict(input_frame)
+        print(f"Prediction score: {prediction[0][0]}")
+
+        color = (0, 255, 0)
+        text = "No Violence Detected"
+        if prediction[0][0] > 0.5:
+            color = (0, 0, 255)
+            text = "Violence Detected!"
+
+        cv2.rectangle(frame, (0, 0), (frame.shape[1], frame.shape[0]), color, 10)
+        cv2.putText(frame, text, (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, color, 2)
+
+        ret, buffer = cv2.imencode('.jpg', frame)
+        frame = buffer.tobytes()
+        yield (b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
